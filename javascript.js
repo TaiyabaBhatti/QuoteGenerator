be89beb93cd355cad4591e4eb451a3cfdf5a5778
TaiyabaBhatti <taiyababhattii@gmail.com>
2024-05-26 13:46:01 +0500

    functionality for fetching api data as quotes and author name

--- javascript.js
+++ javascript.js
@@ -1,3 +1,27 @@
 const button = document.getElementById("generate-btn");
 const quote=document.getElementById("quote");
 const author=document.getElementById("author");
+
+// url of the api end point
+const api="https://api.quotable.io/random";
+button.addEventListener("click",()=>{
+    fetch(api)
+    .then((response) => response.json())
+    .then((data) => {
+
+quote.textContent=`"${data.content}"`;
+author.textContent=`-${data.author}`;
+
+    })
+    .catch((error) => {
+
+        console.error("Error fetching the quote:", error);
+    });
+
+
+
+
+});
+
+
+
